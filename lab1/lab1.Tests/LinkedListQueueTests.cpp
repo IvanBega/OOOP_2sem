d6047c592e51b008939dda340bbd13513f271c40
Ivan Bega <bega.ivan03@gmail.com>
2022-01-31 11:24:58 +0200

    Unit Tests for LinkedListQueue

---
+++ lab1/lab1.Tests/LinkedListQueueTests.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LinkedListQueue.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab1Tests
+{
+	TEST_CLASS(LinkedListQueueTests)
+	{
+	public:
+		
+		TEST_METHOD(AddAndPopOneElement)
+		{
+			auto q = new LinkedListQueue<int>;
+			q->push(100, 1);
+			int pop = q->pop();
+			Assert::AreEqual(100, pop);
+		}
+		TEST_METHOD(AddAndSeekOneElement)
+		{
+			auto q = new LinkedListQueue<int>;
+			q->push(100, 1);
+			int pop = q->seek();
+			Assert::AreEqual(100, pop);
+		}
+		TEST_METHOD(AddAndPopElementsSamePriority)
+		{
+			int values[10] = { 10,9,8,7,6,5,4,3,2,1 };
+			auto q = new LinkedListQueue<int>;
+			for (int i = 0; i < 10; i++)
+			{
+				q->push(values[i], 0);
+			}
+			for (int i = 0; i < 10; i++)
+			{
+				Assert::AreEqual(values[i], q->pop());
+			}
+		}
+		TEST_METHOD(AddAndPopManyElements)
+		{
+			auto q = new LinkedListQueue<int>;
+			for (int i = 0; i < 1000; i++)
+			{
+				q->push(i, 1000 - i);
+			}
+			for (int i = 0; i < 1000; i++)
+			{
+				Assert::AreEqual(1000 - i - 1, q->pop());
+			}
+			
+		}
+	};
+}

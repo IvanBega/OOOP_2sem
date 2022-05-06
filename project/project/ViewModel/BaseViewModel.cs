a5401970f127b2f7647d09b7ecc51733d592bc3d
Ivan Bega <bega.ivan03@gmail.com>
2022-05-06 19:24:03 +0300

    project files, MainViewModel

---
+++ project/project/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace project.ViewModel
+{
+    public class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}

99174e535b231dfe3f31bebce329ec7e5513447b
Ivan Bega <bega.ivan03@gmail.com>
2022-05-08 15:18:12 +0300

    ExercisePopup backend

--- project/project/ViewModel/ExercisePopupViewModel.cs
+++ project/project/ViewModel/ExercisePopupViewModel.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace project.ViewModel
 {
     public class ExercisePopupViewModel : BaseViewModel
     {
+        private string entry = "";
+        public Command BackButtonCommand { get; set; }
+        public Command ConfirmCommand { get; set; }
+        public string Entry
+        {
+            get { return entry; }
+            set { entry = value; NotifyPropertyChanged(); }
+        }
         public ExercisePopupViewModel()
         {
-
+            BackButtonCommand = new Command(BackButtonClicked);
+            ConfirmCommand = new Command(ConfirmClicked);
         }
+
+        private async void ConfirmClicked(object obj)
+        {
+            MainViewModel.AddExercise(Entry);
+            await PopupNavigation.Instance.PopAsync(false);
+        }
+
+        private async void BackButtonClicked(object obj)
+        {
+            await PopupNavigation.Instance.PopAsync(false);
+        }
+
+        
     }
 }
--- project/project/ViewModel/MainViewModel.cs
+++ project/project/ViewModel/MainViewModel.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 using project.Views;
+using project.Model;
+
 namespace project.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public static List<ExerciseModel> ExerciseList { get; set; }
         public MainViewModel()
         {
             ExerciseCommand = new Command(ExerciseClicked);
+            ExerciseList = new List<ExerciseModel>();
 
         }
         public Command ExerciseCommand { get; set; }
@@ -17,6 +21,25 @@ namespace project.ViewModel
         {
             Application.Current.MainPage.Navigation.PushAsync(new ExerciseListPage());
             //Navigation
+        }
+        public static void AddExercise(string entry)
+        {
+            ExerciseModel newExercise = new ExerciseModel();
+            newExercise.Name = entry;
+
+            List<LogModel> newData = new List<LogModel>();
+            LogModel data = new LogModel();
+            data.Reps = 0;
+            data.Sets = 0;
+            data.Weights = 0;
+            data.Date = DateTime.Now.ToString("dd/MM/yy");
+            newData.Add(data);
+
+            newExercise.Data = newData;
+
+            ExerciseList.Add(newExercise);
+            App.Current.MainPage.Navigation.PopToRootAsync();
+            Application.Current.MainPage.Navigation.PushAsync(new Views.ExerciseListPage());
         }
     }
 }

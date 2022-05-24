c466ae181f1f6967387f690d3398f63090b91d10
Ivan Bega <bega.ivan03@gmail.com>
2022-05-24 18:23:08 +0300

    add LogPopup

---
+++ project/project/ViewModel/LogPopupViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project.ViewModel
+{
+    public class LogPopupViewModel
+    {
+        //private double entryReps1 = 0;
+        //private double entryReps2 = 0;
+        //private double entryReps3 = 0;
+        //private double entryReps4 = 0;
+        //private double entryReps5 = 0;
+        //private double entryWeight1 = 0;
+        //private double entryWeight2 = 0;
+        //private double entryWeight3 = 0;
+        //private double entryWeight4 = 0;
+        //private double entryWeight5 = 0;
+        public LogPopupViewModel()
+        {
+
+        }
+    }
+}
---
+++ project/project/Views/Popups/LogPopup.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace project.Views.Popups
+{
+    public partial class LogPopup : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public LogPopup()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel.LogPopupViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+        protected override void OnAppearingAnimationBegin()
+        {
+            base.OnAppearingAnimationBegin();
+        }
+        protected override void OnAppearingAnimationEnd()
+        {
+            base.OnAppearingAnimationEnd();
+        }
+
+        protected override void OnDisappearingAnimationBegin()
+        {
+            base.OnDisappearingAnimationBegin();
+        }
+
+        protected override void OnDisappearingAnimationEnd()
+        {
+            base.OnDisappearingAnimationEnd();
+        }
+
+        protected override Task OnAppearingAnimationBeginAsync()
+        {
+            return base.OnAppearingAnimationBeginAsync();
+        }
+
+        protected override Task OnAppearingAnimationEndAsync()
+        {
+            return base.OnAppearingAnimationEndAsync();
+        }
+
+        protected override Task OnDisappearingAnimationBeginAsync()
+        {
+            return base.OnDisappearingAnimationBeginAsync();
+        }
+
+        protected override Task OnDisappearingAnimationEndAsync()
+        {
+            return base.OnDisappearingAnimationEndAsync();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return base.OnBackButtonPressed();
+        }
+
+        protected override bool OnBackgroundClicked()
+        {
+            return base.OnBackgroundClicked();
+        }
+    }
+}

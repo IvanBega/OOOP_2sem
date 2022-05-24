eeda7043e090fdcb3e39d5e6db8f8553e3c6589b
Ivan Bega <bega.ivan03@gmail.com>
2022-05-24 18:17:30 +0300

    menu for each Exercise

---
+++ project/project/ViewModel/ExerciseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace project.ViewModel
+{
+    public class ExerciseViewModel : BaseViewModel
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; NotifyPropertyChanged(); }
+        }
+        public Command BackButtonCommand;
+        public ExerciseViewModel()
+        {
+            BackButtonCommand = new Command(BackButtonClicked);
+        }
+
+        private void BackButtonClicked(object obj)
+        {
+            Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
--- project/project/Views/ExerciseListPage.xaml.cs
+++ project/project/Views/ExerciseListPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using project.Model;
 using project.ViewModel;
+using project.Views.Popups;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +18,15 @@ namespace project.Views
         {
             InitializeComponent();
             BindingContext = new ViewModel.ExerciseListViewModel();
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+            ((ListView)sender).SelectedItem = null; // deselect
+            var content = e.Item as ExerciseModel;
+            await Application.Current.MainPage.Navigation.PushAsync(new ExercisePage());
         }
     }
 }

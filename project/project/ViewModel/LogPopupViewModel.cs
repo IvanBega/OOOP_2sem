4ffa5544fc02c74fd4635ea3d929532554c38a20
Ivan Bega <bega.ivan03@gmail.com>
2022-05-26 17:29:58 +0300

    LogPopup markup

--- project/project/ViewModel/LogPopupViewModel.cs
+++ project/project/ViewModel/LogPopupViewModel.cs
@@ -1,24 +1,28 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace project.ViewModel
 {
     public class LogPopupViewModel
     {
-        //private double entryReps1 = 0;
-        //private double entryReps2 = 0;
-        //private double entryReps3 = 0;
-        //private double entryReps4 = 0;
-        //private double entryReps5 = 0;
-        //private double entryWeight1 = 0;
-        //private double entryWeight2 = 0;
-        //private double entryWeight3 = 0;
-        //private double entryWeight4 = 0;
-        //private double entryWeight5 = 0;
+        private double EntryReps1 = 0;
+        private double EntryWeights1 = 0;
+        private double EntryReps2 = 0;
+        private double EntryWeights2 = 0;
+        public string LogDate { get; set; }
+        public Command BackButtonCommand { get; set; }
         public LogPopupViewModel()
         {
+            LogDate = DateTime.Now.ToString("dd/MM/yy");
+            BackButtonCommand = new Command(BackButtonClicked);
+        }
 
+        private async void BackButtonClicked()
+        {
+            await PopupNavigation.Instance.PopAsync();
         }
     }
 }
